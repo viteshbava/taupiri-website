9a1e96adb953515640496e8521bcbbba6ce19e76
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-09-26 18:45:41 +1000

    Reset page

--- src/app/poutama/page.tsx
+++ src/app/poutama/page.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 
 const page = () => {
-  return (
-    <div
-      className="w-screen h-screen"
-      style={{
-        backgroundImage: `url('/poutama-final.svg')`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: '#430C08'
-      }}
-    >
-    </div>
-  );
+  return <></>;
 };
 
 export default page;
